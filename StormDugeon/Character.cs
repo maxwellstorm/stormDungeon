d8d1bf36928b77d97c614e2c1ffca3d7df59a9cc
maxwellstorm <mxs5194@rit.edu>
2015-02-12 13:32:52 -0500

    recommit

---
+++ StormDugeon/Character.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StormDugeon
+{
+    class Character
+    {
+        private CharacterClass CharClass;
+        private List<Perk> SelectedPerk;
+        private List<Ability> SelectedAbilies;
+        private List<Perk> LockedPerks;
+        private List<Ability> LockedAbilities;
+        private string Name;
+       
+
+        //equipment
+        private Weapon EquippedWeapon;
+        private Armour EquippedArmour;
+
+        //stats
+        private int Level;
+        private double Health;
+        private int Armour;
+        private double Damage;
+        private int Strength;
+        private int Intellegence;
+        private int Dextreity;
+        private int Survivaiblty;
+        private int Speed;
+        
+  
+
+        private const double StrengthDamageModifier = .1;
+        private const double SurvivabiltyHealthModifier = .1;
+
+
+        public void SetStrength(int s)
+        {
+            Strength = s;
+            Damage = Damage + (s * StrengthDamageModifier); //add 10% to damage for each strength point
+        }
+
+        public void SetSurvivabiltiy(int s)
+        {
+            Survivaiblty = s;
+            Health = Health + (s * SurvivabiltyHealthModifier);
+        }
+
+
+      
+   }
+}
---
+++ StormDugeon/CharacterClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StormDugeon
+{
+    interface CharacterClass//should this be a classor an interface?
+    {
+         string className{ get; set;}
+         double Health{ get; set;}
+         string model { get; set; }
+         int Armour { get; set; }
+         double Damage { get; set; }
+         int Strength { get; set; }
+         int Intellegence { get; set; }
+         int Dextreity { get; set; }
+         int Survivaiblty { get; set; }
+         int Speed { get; set; }
+         List<Ability> StartingAbilities { get; set; }
+         void RandomizeAbilites();//randomize starting abiltiies
+        
+    }
+}
